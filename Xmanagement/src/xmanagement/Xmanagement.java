43a6c7a9510665428f1822341b74747208196bff
lyr03 <lyr03@DESKTOP-IGIAOFS>
2023-03-16 16:21:22 +0900

    init commit

---
+++ Xmanagement/src/xmanagement/Xmanagement.java
@@ -0,0 +1,14 @@
+package xmanagement;
+
+public class Xmanagement {
+	
+	public static void main(String[] args) {
+		System.out.println("1. Add Photo");
+		System.out.println("2. Delete Student");
+		System.out.println("3. Edit Photo");
+		System.out.println("4. View Photo");
+		System.err.println("5. Exit");
+	}
+	
+
+}
