f19b1ef3f2bda7d128f6451123789a021d0dfd13
lyr03 <lyr03@DESKTOP-IGIAOFS>
2023-03-21 14:55:13 +0900

    young

--- a/src/a/D.java
+++ a/src/a/D.java
@@ -2,7 +2,11 @@ package a;
 import java.util.Scanner;
 public class D {
 	public static void main(String[]arg) {
-		
+		int i =0;
+		while (i<3) {
+			System.out.println("Rule #"+i);
+			i = i+1;
+		}
 	}
 
 }
